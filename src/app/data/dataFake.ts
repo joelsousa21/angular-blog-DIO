544bca059dd7b9f14ac8a67a4b2a2f651df03340
joelsousa21 <joelsousa21@outlook.com>
2023-10-15 16:50:33 -0300

    Conclusão do desafio blog com Angular pela DIO

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,27 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "'O BRABO' um doutor supremo, conduz Gerald de Rívea, um novato promissor, rumo ao sucesso.",
+    "description": "Em um ato de reverência e humildade, Gerald de Rívea ousou confessar a 'O BRABO' que ele, um simples novato, pode não estar à altura de sua grandiosa mentoria.",
+    "photoCover":"./assets/images/mentor_do_geraldao.jpeg"
   },
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
-  }
+    "title": "Gandalf reverencia 'O BRABO' por ter iluminado sua vida com uma sabedoria que transcende o tempo.",
+    "description": "Gandalf se transforma drasticamente em apenas 10 minutos de mentoria com 'O BRABO', exibindo uma mente afiada como nunca antes.",
+    "photoCover":"./assets/images/mentor_do_gandalf.jpeg"
+  },
+  {
+    "id":"3",
+    "title": "Mentoria de 'O BRABO' para os 'Peaky Blunders' vira manchete mundial.",
+    "description": "Em todo o mundo, a mentoria de 'O BRABO' é cobiçada, com muitos dispostos a pagar fortunas, mesmo sabendo que 'O BRABO' não necessita.",
+    "photoCover":"./assets/images/mentor_dos_peaky_blinders.jpeg"
+  },
+  {
+    "id":"4",
+    "title": "Inúmeras produtoras de renome buscaram o Soldado Invernal após a revelação de sua parceria com 'O BRABO'.",
+    "description": "Após a mentoria de 'O BRABO', o Soldado Invernal se sente confiante em embarcar em novas aventuras nas telas de cinema.",
+    "photoCover":"./assets/images/mentor_do_soldado_invernal.jpeg"
+  },
+  
 ]
